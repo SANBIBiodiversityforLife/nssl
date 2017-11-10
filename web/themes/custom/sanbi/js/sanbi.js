88adceebe61ee6287708aa7a1fe9d35ab04f9c75
Rukaya <r.johaadien@sanbi.org.za>
2017-11-10 10:17:56 +0200

    Fixes #58

--- web/themes/custom/sanbi/js/sanbi.js
+++ web/themes/custom/sanbi/js/sanbi.js
@@ -14,14 +14,8 @@ jQuery(document).ready(function($){
 	
 	// Tried to do this in the template but to no avail. 
 	$('#sspFlag').appendTo("h1.page-header");
-	if ($('.field--name-field-sensitivity .related-species-placeholder').length > 0) {
-		$('.field--name-field-sensitivity .related-species-placeholder').replaceWith($('.field--name-field-similar-species'));
-	}
 	
-	// Hide the similar species field if required
-	$('#edit-field-similar-species-wrapper').hide();
 	if($('#edit-field-exploitation-extent input:checked').val() == 'uncertain') {
-		$('#edit-field-similar-species-wrapper').show();
 		$('a[href="#edit-group-demographics-nav').parent().hide();	
 	}
 	else if($('#edit-field-exploitation-extent input:checked').val() == 'none') {
@@ -41,10 +35,7 @@ jQuery(document).ready(function($){
 			});
 			$("#cke_edit-field-pop-vulnerability-refs-0-value iframe").contents().find("body").html('');
 			
-			if(this.value == 'none') {
-				$('#edit-field-similar-species-wrapper').hide('fast');	
-				$('#edit-field-similar-species-wrapper input').val("");	
-				
+			if(this.value == 'none') {				
 				// If a species is not exploited one should not fill in any other criteria. 
 				$('a[href="#edit-group-pop-size-nav').parent().hide('fast');	
 				$('a[href="#edit-group-regeneration-potential').parent().hide('fast');	
@@ -55,8 +46,6 @@ jQuery(document).ready(function($){
 		}
 		else {
 			$('a[href="#edit-group-demographics-nav').parent().show('fast');
-			$('#edit-field-similar-species-wrapper').hide('fast');
-			$('#edit-field-similar-species-wrapper input').val("");
 		}
 	});
 });
