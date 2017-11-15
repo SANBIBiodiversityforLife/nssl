2b6735d7c6ccf09b19e3363c890ffea504879708
Rukaya <r.johaadien@sanbi.org.za>
2017-11-15 11:31:07 +0200

    Fixes #65 and makes the language used a bit clearer again

--- web/modules/custom/ssp_import/src/Controller/SspImportController.php
+++ web/modules/custom/ssp_import/src/Controller/SspImportController.php
@@ -8,78 +8,31 @@ use Drupal\node\Entity\Node;
 class SspImportController {
   public function content() {
     $row = 1;
-	/* 
-	// Plants
-	if (($handle = fopen("http://nssl.sanbi.org.za/sites/default/files/ssp.csv", "r")) !== FALSE) {
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			if($row > 1) {
+	
+	// Plants and animals are imported from this spreadsheet
+	// Format: Group,Family,Species,Non-assessed status,Author UID
+	if(($handle = fopen("http://nssl.sanbi.org.za/sites/default/files/species_for_import.csv", "r")) !== FALSE) {
+		while(($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			if($row > 1) { // Skip the header row
 				$node = Node::create(array(
 					'type' => 'species',
-					'title' => $data[0],
+					'title' => $data[2],
 					'langcode' => 'en',
-					'uid' => '1',
+					'uid' => $data[4],
 					'status' => 0,
 				));
-				$node->field_type = 'Plant';
+				$node->field_family = $data[1];
+				$node->field_type = $data[0];
 				
-				// Family
-				$node->field_type = $data[20];
-				
-				// Past justification
-				$node->field_justification = $data[1] . ' / ' . $data[2];
-				
-				// Standardise range size format
-				$range = str_replace('<', '', str_replace('>', '', $data[6]));
-				$matched = preg_match('\s*(\d+)\s*-\s*\d+', $range, $matches);
-				if($matched == 1) {
-					$range = $matches[1];
+				// Not assessed status
+				if($data[3] == 'Not sensitive') {
+					$node->field_sensitivity->value = 'preliminary_not_sensitive';
+					$node->field_is_sensitive->value = 0;
+				} else {
+					$node->field_sensitivity->value = 'preliminary_sensitive';
+					$node->field_is_sensitive->value = 1;
 				}
 				
-				// Standardise population size format
-				$pop_size =  str_replace('<', '', str_replace('>', '', $data[7]));
-				$matched = preg_match('\s*(\d+)\s*-\s*\d+', $pop_size, $matches);
-				if($matched == 1) {
-					$pop_size = $matches[1];
-				}
-				
-				// Standardise subpopulation number format
-				$sub_pop_number = str_replace('<', '', str_replace('>', '', $data[8]));
-				$matched = preg_match('\s*(\d+)\s*-\s*\d+', $sub_pop_number, $matches);
-				if($matched == 1) {
-					$sub_pop_number = $matches[1];
-				}
-				
-				// Check the criteria for vulnerability
-				$v_refs = [];
-				if(trim($range) != '') {
-					$range = (int) $range;
-					if($range <= 100000) {
-						$node->field_population_vulnerability->value = 'vulnerable';
-						$v_refs[] = 'According to the SANBI Red List, this species has a range of under 100km<sup>2</sup>.';
-					}
-				}
-				if(trim($pop_size) != '') {
-					$pop_size = (int) $pop_size;
-					if($pop_size <= 2500) {
-						$node->field_population_vulnerability->value = 'vulnerable';
-						$v_refs[] = 'According to the SANBI Red List, this species has a pop size of under 2500.';
-					}
-				}
-				if(trim($sub_pop_number) != '') {
-					$sub_pop_number = (int) $sub_pop_number;
-					//kpr('small subpop number ' . $sub_pop_number);
-					//kpr('test = ' . $sub_pop_number <= 5);
-					if($sub_pop_number <= 5) {
-						$node->field_population_vulnerability->value = 'vulnerable';
-						$v_refs[] = 'According to the SANBI Red List, this species has 5 or fewer subpopulations.';
-					}
-				}
-				
-				if(count($v_refs) > 0) {
-					$field_pop_vulnerability_refs = implode(' ', $v_refs);
-				}
-				//kpr($node->field_population_vulnerability->value);
-				//$node->population_vulnerability;
 				$node->save();
 				kpr($data[0]);
 			}
@@ -89,33 +42,10 @@ class SspImportController {
 		fclose($handle);
 	}
 	  	
-	// Animals
-	if (($handle = fopen("http://nssl.sanbi.org.za/sites/default/files/ssp-animals.csv", "r")) !== FALSE) {
-		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-			if($row > 1) {
-				$node = Node::create(array(
-					'type' => 'species',
-					'title' => $data[0],
-					'langcode' => 'en',
-					'uid' => '1',
-					'status' => 0,
-				));
-				$node->field_family = $data[7];
-				$node->field_type = $data[8];
-				$node->field_justification = 'Assessor: ' . $data[1] . ' / Source: ' . $data[3] . ' / Threats: ' . $data[4] . ' + ' . $data[6] . ' / Motivation: ' . $data[5];
-				$node->save();
-				kpr($data[0]);
-			}
-			
-			$row++;
-		}
-		fclose($handle);
-	}
-	*/
 	  
     return array(
       '#type' => 'markup',
-      '#markup' => t('<br><br>Module turned off, nothing imported!'),
+      '#markup' => t('<br><br>Imported ' . ($row - 2)), // Subtracting 2 because we start off as 1 and add 1 at the end 
     );
   }
 }
